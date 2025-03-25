ade7e5eb72026ae8418459078173fdeee7bcf62c
hamzah <hamzahnuralfalah@gmail.com>
2025-03-25 12:52:54 +0700

    update permission contract

--- src/Contracts/Permission.php
+++ src/Contracts/Permission.php
@@ -9,13 +9,14 @@ use Hanafalah\LaravelSupport\Contracts\DataManagement;
 
 interface Permission extends DataManagement
 {
+    public function getPermissionModel(): mixed;
+    public function showUsingRelation(): array;
+    public function getPermission(): mixed;
     public function prepareStorePermission(?array $attributes = null): Model;
     public function prepareViewPermissionList(?array $attributes = null): Collection;
     public function viewPermissionList(): array;
-    public function getPermissionModel(): mixed;
-    public function showUsingRelation(): array;
     public function prepareShowPermission(?Model $model = null, ?array $attributes = null): Model;
     public function showPermission(?Model $model = null): array;
     public function permission(mixed $conditionals = null): Builder;
-    public function addOrChange(?array $attributes = []): self;
+    
 }
--- src/Contracts/Role.php
+++ src/Contracts/Role.php
@@ -2,24 +2,24 @@
 
 namespace Hanafalah\LaravelPermission\Contracts;
 
+use Hanafalah\LaravelPermission\Data\RoleData;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
-use Hanafalah\LaravelPermission\Data\RoleDTO;
 use Hanafalah\LaravelSupport\Contracts\DataManagement;
 
 interface Role extends DataManagement
 {
+    public function showUsingRelation(): array;
+    public function viewUsingRelation(): array;
+    public function getRoleModel(): mixed;
     public function prepareViewRoleList(?array $attributes = null): Collection;
     public function viewRoleList(): array;
-    public function showUsingRelation(): array;
-    public function getRoleModel(): mixed;
     public function prepareShowRole(?Model $model = null, ?array $attributes = null): Model;
     public function showRole(?Model $model = null): array;
-    public function prepareStoreRole(RoleDTO $role_dto): Model;
-    public function storeRole(?array $attributes = null): array;
+    public function prepareStoreRole(RoleData $role_dto): Model;
+    public function storeRole(?RoleData $role_dto = null): array;
     public function prepareDeleteRole(?array $attributes = null): bool;
     public function deleteRole(): bool;
     public function role(mixed $conditionals = null): Builder;
-    public function addOrChange(?array $attributes = []): self;
 }
