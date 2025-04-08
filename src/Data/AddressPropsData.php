a656ef8527c3bbabd6f995cda1cb93008b9f4b64
hamzah <hamzahnuralfalah@gmail.com>
2025-04-08 09:16:15 +0700

    mass update of data (DTO)

--- src/Data/AddressPropsData.php
+++ src/Data/AddressPropsData.php
@@ -8,17 +8,15 @@ use Spatie\LaravelData\Attributes\MapInputName;
 use Spatie\LaravelData\Attributes\MapName;
 
 class AddressPropsData extends Data implements DataAddressPropsData{
-    public function __construct(
-        #[MapName('zip_code')]
-        #[MapInputName('zip_code')]
-        public ?string $zip_code = null, 
+    #[MapName('zip_code')]
+    #[MapInputName('zip_code')]
+    public ?string $zip_code = null;
 
-        #[MapName('rt')]
-        #[MapInputName('rt')]
-        public ?string $rt = null, 
+    #[MapName('rt')]
+    #[MapInputName('rt')]
+    public ?string $rt = null;
 
-        #[MapName('rw')]
-        #[MapInputName('rw')]
-        public ?string $rw = null, 
-    ){}
+    #[MapName('rw')]
+    #[MapInputName('rw')]
+    public ?string $rw = null;
 }
