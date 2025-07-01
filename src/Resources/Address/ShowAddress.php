425fd2f51653e6d7e401b98d7abf206a85aca5ea
Hamzah <hamzahnafalah@gmail.com>
2025-07-01 11:33:52 +0700

    update modules

--- src/Resources/Address/ShowAddress.php
+++ src/Resources/Address/ShowAddress.php
@@ -9,16 +9,16 @@ class ShowAddress extends ViewAddress
     public function toArray(Request $request): array{
         $arr = [
             'province'    => $this->relationValidation('province',function(){
-                return $this->province->toViewApi();
+                return $this->province->toViewApi()->resolve();
             }),
             'district'    => $this->relationValidation('district',function(){
-                return $this->district->toViewApi();
+                return $this->district->toViewApi()->resolve();
             }),
             'subdistrict' => $this->relationValidation('subdistrict',function(){
-                return $this->subdistrict->toViewApi();
+                return $this->subdistrict->toViewApi()->resolve();
             }),
             'village'     => $this->relationValidation('village',function(){
-                return $this->village->toViewApi();
+                return $this->village->toViewApi()->resolve();
             }),
         ];
         return $this->mergeArray(parent::toArray($request),$arr);
