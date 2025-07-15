ad21e48556f31aa2c479b92222ca22326296e309
Hamzah <hamzahnafalah@gmail.com>
2025-07-15 19:02:20 +0700

    update modules

--- src/Models/Role/RoleHasPermission.php
+++ src/Models/Role/RoleHasPermission.php
@@ -3,15 +3,9 @@
 namespace Hanafalah\LaravelPermission\Models\Role;
 
 use Hanafalah\LaravelSupport\Models\BaseModel;
-use Illuminate\Database\Eloquent\Concerns\HasUlids;
 
 class RoleHasPermission extends BaseModel
 {
-    use HasUlids;
-
-    public $incrementing = false;
-    protected $keyType = 'string';
-    protected $primaryKey = 'id';
     protected $list = ['id', 'role_id', 'permission_id'];
 
     public function role(){return $this->belongsToModel('Role');}
