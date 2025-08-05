f93667252ee015d5211b3406af7c9d6476a8b618
Hamzah <hamzahnafalah@gmail.com>
2025-08-05 14:56:51 +0700

    update modules

--- assets/config/config.php
+++ assets/config/config.php
@@ -24,6 +24,7 @@ return [
         ]
     ],
     'commands' => [
-        Commands\InstallMakeCommand::class
+        Commands\InstallMakeCommand::class,
+        Commands\SeedCommand::class
     ]
 ];
